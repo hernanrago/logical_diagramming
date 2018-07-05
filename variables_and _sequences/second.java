f0fbdd67f0954778aa4fd3a32e49c73a8a46ee1f
Hernán <33198151+hernanrago@users.noreply.github.com>
2018-07-05 18:08:04 -0300

    Create second.java

---
+++ variables_and _sequences/second.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Second {
+	public static void main(String[] args) {
+		String firstName;
+		String lastName;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter your first name: ");
+		firstName = scanner.nextLine();
+		
+		System.out.println("Enter your last name: ");
+		lastName = scanner.nextLine();
+		
+		System.out.println(lastName + ", " + firstName;
+	}
+}
